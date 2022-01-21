725f25a6286b0f82121b4047d379d8e79a96fe6a
Aishwarya Singh <aish.akshu@gmail.com>
2022-01-21 21:47:22 +0800

    Add #344

---
+++ Easy/344-Reverse-String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start = 0
+        end = len(s) -1 
+        
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
