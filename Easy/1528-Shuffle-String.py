05c17401ce5999b752c64ee2e0ea66d8ce48f91c
aish21 <aish.akshu@gmail.com>
2022-01-31 10:58:53 +0800

    Add #1528

---
+++ Easy/1528-Shuffle-String.py
@@ -0,0 +1,17 @@
+'''
+You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+'''
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        
+        result = list(s)
+        for i in range(len(indices)):
+            result[indices[i]] = s[i]
+        return "".join(result)
