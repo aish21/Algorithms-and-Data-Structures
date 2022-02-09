e402551fb59982083edeaa52d72588462d038d48
aish21 <aish.akshu@gmail.com>
2022-02-09 10:23:52 +0800

    Add #771

---
+++ Easy/771-Jewels-and-Stones.py
@@ -0,0 +1,19 @@
+'''
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+'''
+
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        count = 0
+        hash_set = set(jewels)
+        for jewel in stones:
+            if jewel in hash_set:
+                count += 1
+        return count
