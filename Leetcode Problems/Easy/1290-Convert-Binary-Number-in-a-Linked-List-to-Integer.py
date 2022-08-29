1f666cba71f8ea93eadc01e4c3ec6ec24a7ffa21
Aishwarya Singh <aish.akshu@gmail.com>
2022-08-29 21:48:53 +0800

    1290 - ez

---
+++ Leetcode Problems/Easy/1290-Convert-Binary-Number-in-a-Linked-List-to-Integer.py
@@ -0,0 +1,22 @@
+'''
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+The most significant bit is at the head of the linked list.
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        result = 0
+        
+        while head:
+            result = result * 2 + head.val
+            head = head.next
+                
+        return result
