dfc00e1076decc03db612a4f743fdd3d378b0af3
Aishwarya Singh <SI0001YA@e.ntu.edu.sg>
2022-09-11 22:57:25 +0800

    706-Design-HashMap.py

---
+++ Leetcode Problems/Easy/706-Design-HashMap.py
@@ -0,0 +1,32 @@
+'''
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+'''
+
+class MyHashMap:
+
+    def __init__(self):
+        self.hashmap = [-1]*10000000
+
+    def put(self, key: int, value: int) -> None:
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        return self.hashmap[key]
+
+    def remove(self, key: int) -> None:
+        self.hashmap[key] = -1
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
