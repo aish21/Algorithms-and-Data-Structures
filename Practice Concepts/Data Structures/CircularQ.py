90129c6cf5aeaab0869054506ca58f655d89e737
aish21 <aish.akshu@gmail.com>
2025-01-05 21:41:31 +0800

    update cq

--- Practice Concepts/Data Structures/CircularQ.py
+++ Practice Concepts/Data Structures/CircularQ.py
@@ -2,58 +2,53 @@
 
 class CircularQ():
 
-    def __init__(self, numElements):
-        self.numElements = numElements
-        self.queue = [None] * numElements
-        self.FRONT = self.REAR = -1
+    def __init__(self, capacity):
+        self.queue = [None] * capacity
+        self.capacity = capacity
+        self.front = 0 
+        self.rear = -1
+        self.size = 0
     
-    def ENQUEUE(self, value):
-        if((self.REAR) + 1 % self.numElements == self.FRONT):
-            print('Circular Queue is full\n')
-        elif(self.FRONT == -1):
-            self.FRONT = 0
-            self.REAR = 0
-            self.queue[self.REAR] = value
-        else:
-            self.REAR = (self.REAR + 1) % self.numElements
-            self.queue[self.REAR] = value
+    def enqueue(self, value):
+        if self.size == self.capacity:
+            raise OverflowError('Queue is full')
+        self.rear = (self.rear + 1) % self.capacity
+        self.queue[self.rear] = value
+        self.size += 1
     
-    def DEQUEUE(self):
-        if(self.FRONT == -1):
-            print('Circular Queue is empty\n')
-        elif(self.FRONT == self.REAR):
-            temp = self.queue[self.FRONT]
-            self.REAR = -1
-            self.FRONT = -1
-            return temp
-        else:
-            temp = self.queue[self.FRONT]
-            self.FRONT = (self.FRONT + 1) % self.numElements
-            return temp
+    def dequeue(self):
+        if self.isEmpty():
+            raise IndexError('Queue is empty')
+        value = self.queue[self.front]
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return value
     
-    def printQ(self):
-        if(self.FRONT == -1):
-            print('Circular Queue is empty\n')
-        elif(self.REAR >= self.FRONT):
-            for i in range(self.FRONT, self.REAR + 1):
-                print(self.queue[i], end = " ")
-                print()
-        else:
-            for i in range(self.FRONT, self.numElements):
-                print(self.queue[i], end = " ")
-            for i in range(0, self.REAR + 1):
-                print(self.queue[i], end = " ")
-            print()
+    def isEmpty(self):
+        return self.size == 0
+    
+    def front(self):
+        if not self.isEmpty():
+            return self.queue[self.front]
+        raise IndexError('Queue is empty')
+    
+    def rear(self):
+        if not self.isEmpty():
+            return self.queue[self.rear]
+        raise IndexError('Queue is empty')
 
-# Driver code
-CircQ = CircularQ(5)
-CircQ.ENQUEUE(1)
-CircQ.ENQUEUE(2)
-CircQ.ENQUEUE(3)
-CircQ.ENQUEUE(4)
-CircQ.ENQUEUE(5)
-print("After ENQUEUE:")
-CircQ.printQ()
-CircQ.DEQUEUE()
-print("After DEQUEUE:")
-CircQ.printQ()
+# Test Cases
+queue = CircularQ(3)
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+assert queue.front() == 1
+assert queue.rear() == 3
+assert queue.dequeue() == 1
+assert queue.front() == 2
+assert queue.rear() == 3
+assert queue.dequeue() == 2
+assert queue.front() == 3
+assert queue.rear() == 3
+assert queue.dequeue() == 3
+assert queue.isEmpty() == True
