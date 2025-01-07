9b2a72e9f6e3cdff2ab1da30ee2dbbcc164204c1
aish21 <aish.akshu@gmail.com>
2025-01-07 22:26:10 +0800

    2704 ez

---
+++ Leetcode Problems/Easy/2704-To-Be-Or-Not-To-Be.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+ *
+ * toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+ * notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+ *
+ * Example 1:
+ *
+ * Input: func = () => expect(5).toBe(5)
+ * Output: {"value": true}
+ * Explanation: 5 === 5 so this expression returns true.
+ *
+ * Example 2:
+ *
+ * Input: func = () => expect(5).toBe(null)
+ * Output: {"error": "Not Equal"}
+ * Explanation: 5 !== null so this expression throw the error "Not Equal".
+ *
+ * Example 3:
+ *
+ * Input: func = () => expect(5).notToBe(null)
+ * Output: {"value": true}
+ * Explanation: 5 !== null so this expression returns true.
+ *
+ */
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  return {
+    toBe: function (val2) {
+      if (val === val2) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: function (val2) {
+      if (val !== val2) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
