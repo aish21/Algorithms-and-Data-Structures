5d44320a70bd01fe9ba06c535b3b4ed3479e5566
aish21 <aish.akshu@gmail.com>
2025-01-07 22:34:45 +0800

    2665 ez

---
+++ Leetcode Problems/Easy/2665-Counter-II.js
@@ -0,0 +1,61 @@
+/**
+ * Write a function createCounter. It should accept an initial integer init.
+ * It should return an object with three functions.
+ *
+ * The three functions are:
+ *
+ * increment() increases the current value by 1 and then returns it.
+ * decrement() reduces the current value by 1 and then returns it.
+ * reset() sets the current value to init and then returns it.
+ *
+ * Example 1:
+ *
+ * Input: init = 5, calls = ["increment","reset","decrement"]
+ * Output: [6,5,4]
+ * Explanation:
+ * const counter = createCounter(5);
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ *
+ * Example 2:
+ *
+ * Input: init = 0, calls = ["increment","increment","decrement","reset","reset"]
+ * Output: [1,2,1,0,0]
+ * Explanation:
+ * const counter = createCounter(0);
+ * counter.increment(); // 1
+ * counter.increment(); // 2
+ * counter.decrement(); // 1
+ * counter.reset(); // 0
+ * counter.reset(); // 0
+ *
+ */
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  let count = init;
+
+  return {
+    increment: function () {
+      return ++count;
+    },
+    decrement: function () {
+      return --count;
+    },
+    reset: function () {
+      count = init;
+      return count;
+    },
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
